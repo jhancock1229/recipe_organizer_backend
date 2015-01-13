43291b3630a841ed1cc89b5e6f6fb91e103b1d6e
Derek Argueta <darguetap@gmail.com>
2015-01-13 14:27:24 -0700

    Updating repo with a separated back and frontends

--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from rest_framework import generics
-from models import *
 from serializers import *
-
-
-def test():
-    pass
 
 
 class RecipeList(generics.ListAPIView):
     serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
 
+
+class IngredientList(generics.ListAPIView):
+    serializer_class = IngredientSerializer
+    queryset = Ingredient.objects.all()
