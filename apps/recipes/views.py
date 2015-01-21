7fd99dca95b1a5cf6163f927d74fa340120248cd
Derek Argueta <darguetap@gmail.com>
2015-01-21 12:04:05 -0700

    add recipes working

--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
 from rest_framework import generics
 from serializers import *
 
 
 class RecipeList(generics.ListAPIView):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+
+
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+
+
+class AddRecipe(generics.CreateAPIView):
     serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
 
