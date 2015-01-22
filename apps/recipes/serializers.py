bc754742687d703720edb7d1c37353df25fc4f98
Bob Ziroll <elderziroll@gmail.com>
2015-01-22 09:27:02 -0700

    Close to getting nested serialization to work in DRF 3 style

--- apps/recipes/serializers.py
+++ apps/recipes/serializers.py
@@ -6,7 +6,7 @@ class IngredientSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ingredient
-        fields = ('name',)
+        # fields = ('name',)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -14,18 +14,18 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ('name', 'description', 'directions', 'ingredients')
+        # fields = ('name', 'description', 'directions', 'ingredients')
 
     def create(self, validated_data):
-        # ingredients_data = validated_data.pop('ingredients')
+        ingredients_data = validated_data.pop('ingredients')
         recipe = Recipe.objects.create(**validated_data)
-        # for ingredient_data in ingredients_data:
-        #     Ingredient.objects.create(**ingredient_data)
+        for ingredient_data in ingredients_data:
+            Ingredient.objects.create(**ingredient_data)
         return recipe
 
 
-class NestedRecipeSerializer(serializers.ModelSerializer):
-    ingredients = IngredientSerializer(many=True)
-
-    class Meta:
-        model = Recipe
+# class NestedRecipeSerializer(serializers.ModelSerializer):
+#     ingredients = IngredientSerializer(many=True)
+#
+#     class Meta:
+#         model = Recipe
